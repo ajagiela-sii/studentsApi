2a97161648bc338f83c753d152a975ed6aac33c7
Aleksandra Jagiela <ajagiela@sii.pl>
2022-12-08 11:32:13 +0100

    added

--- src/test/java/models/Student.java
+++ src/test/java/models/Student.java
@@ -11,11 +11,14 @@ public class Student {
     private String last_name;
     private String date_of_birth;
 
+    private String pesel;
+
     public Student(String first_name, String middle_name, String last_name, String date_of_birth) {
         this.first_name = first_name;
         this.middle_name = middle_name;
         this.last_name = last_name;
         this.date_of_birth = date_of_birth;
+
     }
 
     public Student(){}
